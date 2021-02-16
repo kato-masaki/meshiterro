e685d3dbd8fd27a4ab52f99c8df44ea498c0122c
kato-masaki <0913ritz@gmail.com>
2021-02-16 12:20:40 +0000

    [Finish] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '4f3621dc11f76f61990ab9f597571acdd5775c96b1ea7daa061a68279e7e47fdb4a739ba83ab5a07363ef32070ddb847316cc6a97bba0bf0014f2412157c828f'
