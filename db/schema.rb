b1dfacea0634d97830187569e1a6d26df0bffb8d
kato-masaki <0913ritz@gmail.com>
2021-02-17 14:56:17 +0000

    [Fix] Userモデルにrefile特有の記述を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_17_104315) do
+ActiveRecord::Schema.define(version: 2021_02_17_143029) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
